b79a12480529a43e6178169aced312645f9c8218
SamLee <besamly2018@gmail.com>
2023-05-13 18:22:18 +0900

    Update

--- SharedPacket/SharedPacket/PacketDef.h
+++ SharedPacket/SharedPacket/PacketDef.h
@@ -2,27 +2,21 @@
 
 namespace PacketDef {
 
-	enum REQ_GROUP_ID : int {
-		AUTH = 1,
-		GAME,
-	};
+enum REQ_GROUP_ID : int { AUTH = 1, GAME };
 
-	enum REQ_ID_AUTH : int {
-		LOGIN_REQ = 1,
-		LOGIN_RES,
-	};
+enum REQ_ID_AUTH : int { LOGIN_REQ = 1, LOGIN_RES, CLIENT_DISCONNECTED };
 
-	enum REQ_ID_GAME : int {
-		ENTER_SCENE_REQ = 1,
-		ENTER_SCENE_RES,
-		NPC_LIST_REQ,
-		NPC_LIST_RES,
-		POSITION_UPDATE,
-		MOVE_REQ,
-		MOVE_RES,
-		NPC_SPAWNED_LIST,
-		USE_SKILL_REQ,
-		USE_SKILL_RES
-	};
+enum REQ_ID_GAME : int {
+  ENTER_SCENE_REQ = 1,
+  ENTER_SCENE_RES,
+  NPC_LIST_REQ,
+  NPC_LIST_RES,
+  POSITION_UPDATE,
+  MOVE_REQ,
+  MOVE_RES,
+  NPC_SPAWNED_LIST,
+  USE_SKILL_REQ,
+  USE_SKILL_RES
+};
 
 }  // namespace PacketDef
--- SharedPacket/SharedPacket/Protobuf/proto_files/_compile _for_csharp.bat
+++ SharedPacket/SharedPacket/Protobuf/proto_files/_compile _for_csharp.bat
@@ -1,2 +1,2 @@
-protoc.exe --proto_path=./ --csharp_out=../csharp_output/ *.proto
+protoc.exe --proto_path=./ --csharp_out=../csharp_output/ *.proto --csharp_opt=base_namespace=Protocol
 IF ERRORLEVEL 1 PAUSE
