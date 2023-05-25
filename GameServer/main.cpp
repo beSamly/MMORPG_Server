e2bf484cea6a09b87c7785fed711ac78c3eabcf8
unknown <sangyeonlee@joycity.com>
2023-05-25 17:38:15 +0900

    Dump 기능 추가

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -1,8 +1,13 @@
 #include "pch.h"
 #include "LibraryHeader.h"
 #include "ServerApp.h"
+#include "MinidumpHelp.h"
 
 int main() {
+
+	MinidumpHelp dbgDump;
+	dbgDump.install_self_mini_dump();
+
 	uptr<ServerApp> serverApplication = make_unique<ServerApp>();
 	serverApplication->InitSystems();
 	serverApplication->StartSystems();
